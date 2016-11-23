e11c73ac7d878283c3ac5fa69df822f1a7da82e6
FightingEagle20 <patrick.nanys2000@gmail.com>
2016-11-23 20:35:04 +0100

    Bugfix

--- Test Server/app/routes.js
+++ Test Server/app/routes.js
@@ -25,13 +25,13 @@ module.exports = function(app, passport) {
 		console.log('log request');
 	var inputValue = req.body.submit;
 		
-		if(inputValue="login"){
+		if(inputValue=="login"){
 			console.log('login request');
 		passport.authenticate('local-login', {
 		successRedirect : '/profile', // redirect to the secure profile section
 		failureRedirect : '/login', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
-		}) } else if (inputValue="register") { 
+		}) } else if (inputValue=="register") { 
 		passport.authenticate('local-signup', {
 		successRedirect : '/profile', // redirect to the secure profile section
 		failureRedirect : '/signup', // redirect back to the signup page if there is an error
