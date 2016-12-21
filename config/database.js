578987b631bc5b2380aaffa569eca4b5f46a3e51
adamsch60 <adamschweitzer1@gmail.com>
2016-12-21 13:20:55 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ var sequelize = new Sequelize("roboclash", "server", "pXeQf6EnDdKR", {
 // define the schema for our user model
 var User = sequelize.define('User',
 {
-  username: Sequelize.STRING,
+  email: Sequelize.STRING,
   password: Sequelize.STRING
 })
 
